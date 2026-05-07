02460694b2d67f3593f4868344a0c098ca907546
JBoliveira08 <joao.b.oliveira21@aluno.senai.br>
2026-05-07 11:58:22 -0300

    farmacia_saep

---
+++ src/main/java/com/senai/backend/controle_frequencia/services/MovimentacaoService.java
@@ -0,0 +1,14 @@
+package com.senai.backend.controle_frequencia.services;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovimentacaoService {
+   
+    @Autowired
+    public MovimentacaoService MovimentacaoService;
+
+}
